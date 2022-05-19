f4087b0269b1941d9c0c4a740323e568ded96c42
kosimbekGH <kosimbekkosimov1999@gmail.com>
2022-05-19 11:58:27 +0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-// Import stylesheets
-import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//1. Даны два действительных числа а и Ь. Получить их
+//сумму, разность и произведение.
 
+function getSumDifferenceMultiplyNums(a: number, b: number): {} {
+  let res = {
+    summ: a + b,
+    diff: a - b,
+    mult: a * b,
+  };
+  return res;
+}
+
+getSumDifferenceMultiplyNums(5, 3);
+
+
+//2. Даны действительные числа х и у. Получить
+// |x|-|y|/1+|x*y|
+
+function getRealNumbersXY(x: number, y: number): number {
+return
+}
